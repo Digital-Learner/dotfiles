1f7a94901b1d262ac26e15b63a3a7122af2fa4ef
Digital-Learner <digital.learning.journeys@gmail.com>
2025-05-13 10:51:30 +0100

    Add incremental selection for Treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -19,6 +19,16 @@ return {
       -- auto_install = true,
       highlight = { enable = true },
       indent = { enable = true },
+
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<Enter>", -- set to `false` to disable one of the mappings
+          node_incremental = "<Enter>",
+          scope_incremental = false,
+          node_decremental = "<Backspace>",
+        },
+      },
     })
   end
 }
